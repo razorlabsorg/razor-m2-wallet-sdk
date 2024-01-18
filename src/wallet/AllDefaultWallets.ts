e617f6b9a42b26eb38ca33fe8fb1eb11fc6e2b8b
Nate <89227631+0xNeit@users.noreply.github.com>
2024-01-18 11:21:42 +0100

    Removed unsupported wallets

--- src/wallet/AllDefaultWallets.ts
+++ src/wallet/AllDefaultWallets.ts
@@ -2,20 +2,8 @@ import * as presets from './preset-wallets';
 
 export const AllDefaultWallets = [
   presets.RazorWallet,
-  presets.SuietWallet,
   presets.SuiWallet,
   ...[
     presets.EthosWallet,
-    presets.MartianWallet,
-    presets.SurfWallet,
-    presets.MorphisWallet,
-    presets.GlassWallet,
-    presets.OneKeyWallet,
-    presets.SpacecyWallet,
-    presets.SensuiWallet,
-    presets.NightlyWallet,
-    presets.ElliWallet,
-    presets.TokenPocketWallet,
-    presets.FrontierWallet,
   ].sort((a, b) => (a.name < b.name ? -1 : 1)),
 ];
